2238fb887467146293b2fa780ad6171b6ea78bfb
Kamil Wyszomierski <kamil.wyszomierski+1@gmail.com>
2021-02-22 21:00:23 +0100

    Project setup

---
+++ FunctionalValidator/Source/Main/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  FunctionalValidator
+//
+//  Created by Filo on 22/02/2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+	var window: UIWindow?
+
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+		guard let windowScene = (scene as? UIWindowScene) else { return }
+		let window = UIWindow(windowScene: windowScene)
+		let viewController = ViewController()
+		let navigation = UINavigationController(rootViewController: viewController)
+		window.rootViewController = navigation
+		self.window = window
+		window.makeKeyAndVisible()
+	}
+}
---
+++ FunctionalValidator/Source/Main/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunctionalValidator
+//
+//  Created by Filo on 22/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func loadView() {
+		view = UIView()
+	}
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .red
+	}
+}
